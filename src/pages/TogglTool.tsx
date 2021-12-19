9e042fe535f30db8d7c739b17968340ac9e87435
Fabian Sundholm <fabian.sundholm@hotmail.com>
2021-12-19 04:24:01 +0100

    feat(togglTool): add convert to report csv feature

--- src/pages/TogglTool.tsx
+++ src/pages/TogglTool.tsx
@@ -7,38 +7,25 @@ type MyProps = {
 };
 
 type MyState = {
-    files: [string,TimeEntry[]][]; // like this
-    mergedFile: string[][]
+    files: [string,string[][]][]; // like this
+    mergedFile: string[][],
+    report : string [][]
 };
-
-type TimeEntry = { //TODO: I should probably optimize away this and replace with just string[], unless that makes dynamic columns really hard
-    user: string;
-    email: string;
-    client: string;
-    project: string;
-    task: string;
-    description: string;
-    billable: string;
-    startDate: string;
-    startTime: string;
-    endDate: string;
-    endTime: string;
-    duration: string;
-    tags: string;
-    amount: string;
-}
 
 class TogglTool extends React.Component<MyProps, MyState> {
     constructor(props : MyProps) {
         super(props);
         
         this.setFile = this.setFile.bind(this)
+        this.mergeFiles = this.mergeFiles.bind(this)
+        this.makeReport = this.makeReport.bind(this)
         this.createMergedFile = this.createMergedFile.bind(this)
 
     }
     state: MyState = {
         files : [],
-        mergedFile : []
+        mergedFile : [],
+        report : []
     };
 
     setFile(e : any) {
@@ -50,10 +37,10 @@ class TogglTool extends React.Component<MyProps, MyState> {
                 let result: string = fileReader.result as string
                 let rows : string[] = result.split('\n')
                 rows.pop(); // last element is allways empty row
-                let data : TimeEntry[] = []
+                let data : string[][] = []
                 data = Array.from(new Set(rows)) //make unique
                 .map((row : string) => {
-                    return this.createTimeEntry(row);
+                    return row.split(',');
                 })
                 let header = data[0]; //TODO: this row should probably be used to dynamically set fields according to label
                 data = data.slice(1)
@@ -69,58 +56,56 @@ class TogglTool extends React.Component<MyProps, MyState> {
     }
 
     createMergedFile() {
+        let mergedData = this.mergeFiles()
+        this.setState({
+            mergedFile: mergedData
+        })        
+    }
+
+    mergeFiles() : string[][] {
         let set : Set<string> = new Set()
         Array.from(this.state.files).forEach((file) => {
-            file[1].forEach((entry: TimeEntry) => {
+            file[1].forEach((entry: string[]) => {
                 set.add(JSON.stringify(entry)); //cant find a better way to remove duplicates unfortunately
             })
         })
         let uniqueEntries = Array.from(set)
         let data = uniqueEntries.map((entryString: string) => {
             
-            let entry : TimeEntry = (JSON.parse(entryString) as TimeEntry)
-            return [
-                entry.user,
-                entry.email,
-                entry.client,
-                entry.task,
-                entry.description,
-                entry.billable,
-                entry.startDate,
-                entry.startTime,
-                entry.endDate,
-                entry.endTime,
-                entry.duration,
-                entry.tags,
-                entry.amount,
-                entry.project
-            ]
+            let entry : string[] = (JSON.parse(entryString) as string[])
+            return entry;
         })
-        this.setState({
-            mergedFile: data
+        return data;
+    }
+
+    makeReport() {
+        let map = new Map<string, number>();
+        let mergedFile = this.mergeFiles()
+        mergedFile.forEach((row) => {
+            let duration : [number,number,number] = row[11].split(':').map((str) => +str) as [number,number,number]
+            let durationInseconds = duration[0] * 3600 + duration[1] * 60 + duration[2];
+            if (map.has(row[3])) {
+                map.set(row[3],map.get(row[3]) as number + durationInseconds)
+            } else {
+                map.set(row[3], durationInseconds)
+            }
+
+            let data: string[][] = [];
+            map.forEach((value, key) => {
+                //console.log((value/3600).toString())
+                let hours = Math.floor(value / 3600)
+                let secondsLeft = value - (hours * 3600)
+                let minutes = Math.floor(secondsLeft / 60)
+                secondsLeft = secondsLeft - (minutes * 60)
+                data.push([key, hours + ":" + minutes + ":" + secondsLeft])
+            })
+
+            this.setState({report: data})
+                
         })
     }
 
-    createTimeEntry(text: string) : TimeEntry {
-        //this might be a waste of time
-        let fields: string[] = text.split(',');
-        return {
-            user: fields[0],
-            email: fields[1],
-            client: fields[2],
-            task: fields[3],
-            description: fields[4],
-            billable: fields[5],
-            startDate: fields[6],
-            startTime: fields[7],
-            endDate: fields[8],
-            endTime: fields[9],
-            duration: fields[10],
-            tags: fields[11],
-            amount: fields[12],
-            project: fields[13],
-        }
-    }
+    
 
     render() {
         return (
@@ -145,14 +130,19 @@ class TogglTool extends React.Component<MyProps, MyState> {
                             ?
                             <div className="mt-3" >
                                 <button type="submit" className="btn btn-primary" onClick={this.createMergedFile}>Download Merged</button>
-                                <button type="submit" className="btn btn-primary ml-3">Convert to Report</button>
+                                <button type="submit" className="btn btn-primary ml-3" onClick={this.makeReport}>Convert to Report</button>
                             </div>
                             : <></>
                         }
                         
                         {
                             this.state.mergedFile.length > 0 ? 
-                            <CSVLink data={this.state.mergedFile} enclosingCharacter=''>Download me</CSVLink>
+                            <CSVLink data={this.state.mergedFile} enclosingCharacter='' filename={"merged-entries"}>Download Merged</CSVLink>
+                            : <></>
+                        }
+                        {
+                            this.state.report.length > 0 ? 
+                            <CSVLink data={this.state.report} enclosingCharacter='' filename={"report"}>Download Report</CSVLink>
                             : <></>
                         }
                     </div>
